ace18372682da65dcb17806459611330caa40ab0
Rishi Balapure <rishibalapure12@gmail.com>
2025-01-25 17:06:53 +0530

    Pull this branch for local deploymeny only

--- Transaction_process.py
+++ Transaction_process.py
@@ -1,4 +1,6 @@
-import Key_management
+import Key_Management
+
+from deployment import BlockchainDeployment
 class TransactionProcessing:
     def __init__(self, blockchain_network):
         self.blockchain_network = blockchain_network  # Blockchain instance or API to interact with the blockchain
@@ -7,11 +9,12 @@ class TransactionProcessing:
         # Use the reconstructed key to authorize and sign the transaction
         if not reconstructed_key:
             raise ValueError("Reconstructed key is missing or invalid.")
-
+        blockchaindeployment=BlockchainDeployment()
         # This is a placeholder for the signing process; real implementation will involve cryptographic signing
         signed_transaction = f"Signed({transaction_data}) with key: {reconstructed_key}"
         print("Transaction signed successfully.")
-
+        
+        blockchaindeployment.initiate_transaction_deploy(transaction_data, reconstructed_key)
         # Send the signed transaction to the blockchain
         self.blockchain_network.broadcast_transaction(signed_transaction)
         print("Transaction sent to blockchain.")
@@ -24,19 +27,20 @@ class BlockchainNetwork:
 def transaction_init():
     blockchain_network = BlockchainNetwork()
     transaction_processing = TransactionProcessing(blockchain_network)
-
+    
     # Sample transaction data
     transaction_data = "Transfer 100 tokens to address XYZ"
-    num_shards = 3
-    key_management = Key_management.KeyManagement(num_shards)
-    secret_key_hex = "ABD3410FE"  # Example secret key in hex format
+    num_shards = 5
+    thershold= 5
+    key_management = Key_Management.KeyManagement(num_shards)
+    secret_key_hex = "0xffffffffffffffffffffffffffffffff"  # Example secret key in hex format
 
 # Generate shards
     shards = key_management.generate_key_shards(secret_key_hex)
-    # print("Generated Shards:", shards)
+    print("Generated Shards:", shards)
 
 # Use all generated shards for reconstruction
     reconstructed_key = key_management.reconstruct_key(shards)
     # Assume the key has already been reconstructed
-    transaction_processing.initiate_transaction(transaction_data, reconstructed_key)
+    transaction_processing.initiate_transaction(transaction_data, "0x"+reconstructed_key)
 transaction_init()
